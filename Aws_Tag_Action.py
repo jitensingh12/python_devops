0eae9f50cffa26a0412956b115418e25f338eb2d
jitensingh12 <43533538+jitensingh12@users.noreply.github.com>
2024-11-22 17:21:59 +0530

    Create Aws_Tag_Action.py

---
+++ Aws_Tag_Action.py
@@ -0,0 +1,25 @@
+
+# This is dynamic code just edit function name and action command on ec2 basis and will work as per your need.
+
+def stop_instances():
+    ec2 = boto3.client('ec2')
+    obj = ec2.describe_instances(Filters=[
+                                    {'Name': 'instance-state-name', 'Values': ['stopped']},
+                                    {'Name': 'tag:do_not_stop', 'Values': ['true']}
+                                ])
+    #jj = yaml.safe_load(obj)
+    #print(obj)
+    for reservation in obj['Reservations']:
+        for instance in reservation['Instances']:
+            tags = instance["Tags"]
+            for tag in tags:
+                if((tag['Key'] == 'do_not_stop') and (tag['Value'] == 'true')):
+                    print(instance['InstanceId'], instance['State']['Name'])
+                #print(tag)
+                #print(tag['Value'])
+                #print(tag['Key'])
+            #else:
+            #    print("hello")
+            #    #ec2.stop_instances(InstanceIds=[instance['InstanceId']])
+if __name__ == '__main__':
+    stop_instances()
